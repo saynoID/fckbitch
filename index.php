ffc869489e1c17c27601657f9efee207c10541b5
Sayno <105619680+saynoID@users.noreply.github.com>
2022-06-29 09:45:43 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,149 @@
+<html><head>
+
+<!--------------------------------------------
+
+Hacked syano_id
+
+Script In Owned !!
+
+ 
+
+Contac Me leadsayno@gmail.com
+
+--------------------------------------------->
+
+<title>?</title>
+
+<meta name="Author" content="sayno_id join your system | bwahahahahaaa">
+
+<meta name="copyright" content="sayno_id | bwahahahaaa">
+
+<meta name="description" content="[ SECURITY IS JUST AN ILLUSION ]">
+
+<meta name="keywords" content="Sorry Admin , Your Security Tested By sayno_id">
+
+<script>
+
+var pesen="Fuck Here >_*";
+
+///////////////////////////////////
+
+function clickIE4(){if (event.button==2){alert(pesen);return false;}}
+
+function clickNS4(e){if (document.layers||document.getElementById&&!document.all){if (e.which==2||e.which==3){alert(pesen);return false;}}}
+
+if (document.layers){document.captureEvents(Event.MOUSEDOWN);document.onmousedown=clickNS4;}
+
+else if (document.all&&!document.getElementById){document.onmousedown=clickIE4;}
+
+document.oncontextmenu=new Function("alert(pesen);return false")
+
+</script>
+
+<style>
+
+*{
+
+    margin-top:2px;
+
+}
+
+ 
+
+body{
+
+    background-position: center;
+
+    background-color:#000000;
+
+    height:98%;
+
+    width:99%;
+
+    background-attachment: fixed;
+
+    background-size:100% 117%;
+
+    background-image:url(https://wallpaperscraft.com/image/guy_anime_computer_tears_sadness_room_96990_1920x1080.jpg);
+
+    cursor: url(http://cur.cursors-4u.net/cursors/cur-11/cur1025.ani), url(http://cur.cursors-4u.net/cursors/cur-11/cur1025.png), progress !important;
+
+}
+
+.foter span{
+
+    text-align: center;
+
+    font-family: Orbitron;
+
+    font-size: 300%;
+
+    color: silver;
+
+    text-shadow: 0 0 3px silver, 0px 0px 5px darkblue;
+
+}
+
+ 
+
+#sec:hover{
+
+    color:#000000;
+
+    text-shadow: 0 0 3px silver, 0px 0px 5px darkblue;
+
+}
+
+ 
+
+</style></head>
+
+<!-- Cursor Exs .ani , Exs.jpg-->
+
+<!--Script Di bwah-->
+
+<body oncontextmenu="return false" onkeydown="return false" onmousedown="return false">
+
+<br><br><br><br><br><br><br><br><br><br>
+
+<!--Tulisan Miring-->
+
+<span id="sec"><div id="foter" class="foter" style="position: absolute; top: 70px; left: -225px; width: 600px; height: 28px; margin: 0px; padding: 10px; font-size: 24px; text-align: center; color: rgb(255, 255, 255); font-family: &quot;trebuchet ms&quot;, Courier new, courier new, sans-serif; transform: rotate(-45deg); transform-origin: 50% 0px 0px; background-color: rgb(0, 0, 0); border: 1px solid rgb(170, 170, 170); opacity: 0.5; z-index: 1200;">
+
+<font face="courier new"><marquee color="darkblue" behavior="Flip" scrollamount="6" width="50%" style="width: 50%;">sayno_id join your system - Security Is Just An Illusion - Indonesia Cyber </marquee></font></div></span>
+
+<!--<iframe width="0" height="0" src="https://www.youtube.com/embed/1-xGerv5FOk?autoplay=1" frameborder="0" allowfullscreen=""></iframe>
+
+---><!--Tulisan Pertama--><center><font size="10" color="darkblue" face="courier new"><span id="sec">H</span><span id="sec">a</span><span id="sec">c</span><span id="sec">k</span><span id="sec">e</span><span id="sec">d</span> <span id="sec">By</span> <span id="sec">sayno</span><span id="sec">_</span><span id="sec">id</span><span id="sec"></span><span id="sec"></span></font>
+
+<br><br>
+
+<!-- Tulisan Kedua--><div id="foter" class="foter">
+
+<span id="sec">S</span><span id="sec">A</span><span id="sec">Y</span><span id="sec">N</span><span id="sec">O</span>
+
+<span id="sec">K</span><span id="sec">H</span><span id="sec">U</span><span id="sec">N</span><span id="sec">:v</span></div>
+
+<!--Tengah--><br><br><br>
+
+<!--Official-->
+
+<font color="darkblue" size="4">
+
+<font color="darkblue" size="5" face="Corier"><font size="5" color="darkblue"><span id="sec">[</span></font><span id="sec">sayno</span> <span id="sec">was</span></font> <span id="sec">HERE</span> <span id="sec">BUAHAHAAAA</span></font><font size="5" color="darkblue"><span id="sec">]</span>
+
+</font>
+
+<br><br><br>
+
+<font color="darkblue" size="4" face="courier new">
+
+<!--Greetz-->
+
+<marquee color="darkblue" behavior="alternate" scrollamount="6" width="70%" style="width: 70%;">&lt;<font color="darkblue">-</font><font color="darkblue">-</font>&gt; IdaRahma <font color="darkblue">-</font> Kill Me <font color="darkblue">-</font> Broken Angel <font color="darkblue">-</font> RAN26 <font color="darkblue">-</font> ./Mr.Blank007 <font color="darkblue">-</font> <font><span id="sec">Mr.x0x</span></font> <font color="darkblue">-</font> PETR03X <font color="darkblue">-</font> VrCy<font color="darkblue">-</font>01` <font color="darkblue">-</font> J4NC0K <font color="darkblue">-</font> FRU_403 <font color="darkblue">-</font> kasian <font color="darkblue">-</font> MRY <font color="darkblue">-</font> siluman.nanas <font color="darkblue">-</font> /mr.Andro0TFL <font color="darkblue">-</font> Triple A <font color="darkblue">-</font> Noob_Orb <font color="darkblue">-</font> BOCAH KRIMINAL <font color="darkblue">-</font> r007@unkn0wn <font color="darkblue">-</font> MrX404 <font color="darkblue">-</font> Mister$ <font color="darkblue">-</font> AbsTrax <font color="darkblue">-</font> Inisial<font color="darkblue">-</font>C <font color="darkblue">-</font> user98 <font color="darkblue">-</font> JerukID27 <font color="darkblue">-</font> TinamaT_TeaM <font color="darkblue">-</font> Mr. S3PUL0H <font color="darkblue">-</font> MR.U13 <font color="darkblue">-</font> Dark_Oracle <font color="darkblue">-</font> SCYTHE404_LOL <font color="darkblue">-</font> Mr.Cr4bs <font color="darkblue">-</font> SexyGirl <font color="darkblue">-</font><font color="darkblue">-</font><font color="darkblue">DziUnincode</font>&gt;</marquee>
+
+</font><br><br><br><br>
+
+</center
+
+<!--Copyright_@ceyno 2022></font></body></html>
